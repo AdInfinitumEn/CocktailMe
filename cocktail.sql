0e122199478de86022e0ab7e1c64ae84b686f0d8
Demirkol, Goerkem <gdemirkol315@gmail.com>
2021-01-21 23:10:16 +0100

    cocktails adjusted

--- cocktail.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Cocktails
-(
-ID varchar primary key,
-Name varchar (255),
-IngrID varchar foreign key,
-);
---
+++ src/cocktail.sql
@@ -0,0 +1,6 @@
+CREATE TABLE COCKTAILS
+(
+ID varchar primary key,
+NAME varchar (255),
+INGR_ID varchar foreign key,
+);
